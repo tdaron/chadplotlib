b5f6d6162f7f7d8dad33c23d969268c677c70815
Théo Daron <theo@daron.be>
2024-05-21 11:50:32 +0200

    basic gigachad

---
+++ README.md
@@ -0,0 +1,19 @@
+# Chadplotlib
+
+## Basic usage
+
+`pip install chadplotlib`
+
+```python
+import chadplotlib
+chadplotlib.makeMeAChad()
+```
+
+## Compiling from scratch 
+Building:
+
+`python3 -m build`
+
+Installing:
+
+`pip install dist/*.whl`
---
+++ src/chadplotlib/__init__.py
@@ -0,0 +1,20 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+import importlib
+
+def makeMeAChad():
+    font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
+    path = importlib.resources.files(__name__).joinpath("fonts/Montserrat-Bold.ttf")
+    font_manager.fontManager.addfont(path=path)
+    path = importlib.resources.files(__name__).joinpath("fonts/Montserrat-Medium.ttf")
+    font_manager.fontManager.addfont(path=path)
+    path = importlib.resources.files(__name__).joinpath("fonts/Montserrat-Regular.ttf")
+    font_manager.fontManager.addfont(path=path)
+    mpl.rcParams['font.family'] = 'Montserrat'
+    mpl.rcParams['font.weight'] = 800
+    plt.rcParams['figure.facecolor'] = '#f0f0f0'
+    plt.rcParams['axes.facecolor'] = '#f0f0f0'
+    plt.rcParams.update({'font.size': 13})
+    print('chad setup ok')
+
